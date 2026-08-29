fa379d8e5470140c7dcd82c8ae1ee16e4a08dc44
Aleksandr <sasha5nemov5@gmail.com>
2026-08-29 18:48:29 +0300

    123

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -2,6 +2,7 @@ import Button from "./Button"
 import CountCycle from "./CountCycle"
 import Music from "./Music"
 import '../styles/Control.css'
+import styles from"../styles/Buttons.module.css"
 import { clsx } from 'clsx';
 
 const Control = ({startTimer, cycle, changeCycle, clickNextButton, clickBackButton, rengeVolume, isRunning}) => {
@@ -10,13 +11,13 @@ const Control = ({startTimer, cycle, changeCycle, clickNextButton, clickBackButt
         <div className="control">
             <Music rengeVolume = {rengeVolume}/>
             <div className="control-buttons">
-                <Button className="reverse-button" onClick = {clickBackButton}/>
-                <Button className={clsx({
-                        'play-button' : isRunning === false,
-                        'pause-button' : isRunning === true
+                <Button className={`${styles.button} ${styles.reverse} ${styles.icon}`} onClick = {clickBackButton}/>
+                <Button className={clsx(`${styles.button} ${styles.icon}`,{
+                        [styles.play] : isRunning === false,
+                        [styles.pause] : isRunning === true
                     })} 
                     onClick = {startTimer}/>
-                <Button className="next-button"  onClick = {clickNextButton}/>
+                <Button className= {`${styles.button} ${styles.next} ${styles.icon}`} onClick = {clickNextButton}/>
             </div>
             <CountCycle cycle = {cycle} changeCycle = {changeCycle}/>
         </div>
